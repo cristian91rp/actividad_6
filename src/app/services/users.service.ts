a86c8f534742815d13ac9bd18a75bdee1a8fe672
Cristian Requena <cristian91rp@gmail.com>
2024-03-16 15:03:52 +0100

    Creado el diseño de la botonera
    se ha añadido la función delete a user.service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { IUser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+  private httpClient = inject(HttpClient)
 
-  constructor() { }
+  private baseUrl = 'https://peticiones.online/api/users' 
+  
+
+
+  delete(_id: string) : Promise<IUser>{
+    return lastValueFrom(this.httpClient.delete<IUser>(`${this.baseUrl}/${_id}`))
+  }
 }
