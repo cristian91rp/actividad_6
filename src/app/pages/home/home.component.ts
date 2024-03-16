cf0bf7f45bb1a891d01cdc49c40378014819b37e
Cristian Requena <cristian91rp@gmail.com>
2024-03-16 15:29:16 +0100

    Creado diseño de Home
    Se ha modificado el tamaño de las imágenes de usurarios en user-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
+import { IUser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  usuarios: IUser[]= []
+  userServices = inject(UsersService)
 
+
+  async ngOnInit(): Promise<void> {
+    
+    try{
+      let data = await this.userServices.getAllPromises()
+      this.usuarios = data.results
+      console.log(this.usuarios)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { IUser } from '../interfaces/iuser.interface';
 import { lastValueFrom } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { IData } from '../interfaces/idata.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class UsersService {
   private httpClient = inject(HttpClient)
 
   private baseUrl = 'https://peticiones.online/api/users' 
-  
 
+  
+  getAllPromises(): Promise<IData> {
+    return lastValueFrom(this.httpClient.get<IData>(this.baseUrl))
+  }
 
   delete(_id: string) : Promise<IUser>{
     return lastValueFrom(this.httpClient.delete<IUser>(`${this.baseUrl}/${_id}`))
