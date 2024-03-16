a4a342c3aa9f3fb86c00ded9ad27b3e5d11070f1
Cristian Requena <cristian91rp@gmail.com>
2024-03-16 14:39:07 +0100

    Añadida el header en el Html
    Se ha añadido librería Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FormUserComponent } from './pages/form-user/form-user.component';
+import { ViewUserComponent } from './pages/view-user/view-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: 'full', redirectTo: ""},
+    {path: "", component: HomeComponent},
+    {path: 'newuser', component: FormUserComponent},
+    {path: 'user/:_id', component: ViewUserComponent},
+    {path: 'user/update/:_id', component: FormUserComponent},
+    {path: '**', redirectTo: ""},
+];
+
