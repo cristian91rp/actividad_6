3c9220c3444e505e29ee8f7d8b070fbc086e76e9
Cristian Requena <cristian91rp@gmail.com>
2024-03-16 12:48:08 +0100

    Creación de components, interfaces, pages y services

---
+++ src/app/components/buttonbox/buttonbox.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttonbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttonbox.component.html',
+  styleUrl: './buttonbox.component.css'
+})
+export class ButtonboxComponent {
+
+}
